eda4ee4074c5ab2f932d2b0c4456967826c677c6
Matthew <116422904+matthewridgwaydeximo@users.noreply.github.com>
2024-07-14 20:46:49 +0800

    Add Binary search implementation

--- BinarySearch.ts
+++ BinarySearch.ts
@@ -1,1 +1,20 @@
+function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length - 1;
 
+    do {
+        let mid = lo + Math.floor((hi - lo) / 2);
+
+        console.log(mid);
+
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] < needle) {
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    } while (lo <= hi);
+
+    return false;
+}
