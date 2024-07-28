ecaca15afa8908279e1ff4b812d652f11a81556a
Matthew <matthewridgway.deximo@gmail.com>
2024-07-28 18:30:00 +0000

    WIP TwoCrystalBalls problem (Sunday)

--- src/TwoCrystalBalls.ts
+++ src/TwoCrystalBalls.ts
@@ -1,1 +1,9 @@
-export default function twoCrystalBalls(breaks: boolean[]): number {}
+export default function twoCrystalBalls(breaks: boolean[]): number {
+    let i = Math.floor(Math.sqrt(breaks.length));
+
+    for (; i < breaks.length; i += Math.floor(Math.sqrt(breaks.length))) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+}
