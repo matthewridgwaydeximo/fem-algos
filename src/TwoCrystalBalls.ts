f45dfa25730da3c918394996af173f5555a0309c
Matthew <116422904+matthewridgwaydeximo@users.noreply.github.com>
2024-07-29 11:03:04 +0800

    Finished TwoCrystalBalls problem

--- src/TwoCrystalBalls.ts
+++ src/TwoCrystalBalls.ts
@@ -1,9 +1,19 @@
 export default function twoCrystalBalls(breaks: boolean[]): number {
-    let i = Math.floor(Math.sqrt(breaks.length));
+    const n = breaks.length;
 
-    for (; i < breaks.length; i += Math.floor(Math.sqrt(breaks.length))) {
-        if (breaks[i]) {
-            break;
-        }
+    if (n === 0) return -1;
+
+    const step = Math.floor(Math.sqrt(n));
+
+    let i = step;
+    for (; i < n; i += step) {
+        if (breaks[i]) break;
     }
+
+    let j = i - step;
+    for (j; j <= i && j < n; j++) {
+        if (breaks[j]) return j;
+    }
+
+    return -1;
 }
